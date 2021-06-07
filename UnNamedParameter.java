24793046430fd350a3c62d478754abbc08b0a075
yash15verma <yash15vermagmail.com>
2021-06-07 21:18:41 +0530

    Get Booking by Id using the UnNamed Parameter

---
+++ UnNamedParameter.java
@@ -0,0 +1,33 @@
+package org.apiAutomation.pathParameters;
+
+import io.restassured.RestAssured;
+
+public class UnNamedParameter {
+    public static void main(String[] args){
+
+        /**
+         * First we need to build the request
+         */
+        RestAssured
+                .given()
+                    .log()
+                    .all()
+
+        /**
+         * Second we need to hit the request and get the response
+         * This is Un-named parameter or In-line parameter
+         * In this URI Parameters, the methods which we mentioned in get method will works based index (works in sequence)
+         */
+                .when()
+                    .get("https://restful-booker.herokuapp.com/{path}/{bookingId}", "booking", 2)
+
+        /**
+         * Third we need to validate the response
+         */
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+
+    }
+}
